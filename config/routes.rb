12b0f3469f2a015f44efe56d366e56a9c8b5ab55
Arabella Stephenson <arabella.stephenson@gmail.com>
2022-02-16 14:01:36 +0000

    finished validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # new route to list restaurants
+  get '/restaurants', to: 'restaurants#index', as: :restaurants
+  # create a new restaurant
+#   get '/restaurants/new', to: 'restaurants#new', as: :new_restaurants
+#   post '/restaurants/', to: 'restaurants#create'
+#   # new route to see specific restaurants
+#   get '/restaurants/:id', to: 'restaurants#show', as: :restaurants
+#   # edit a restaurant
+#   get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurants
+#   patch 'restaurants/:id', to: 'restaurants#update'
+#   # delete a restaurant
+#   delete 'restaurants/:id', to: 'restaurants#destroy'
 end
